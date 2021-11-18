7f4f3d4b23246f3fd5957b626dcc8bc06d93e631
kushidze <megahiman92@gmail.com>
2021-11-19 00:16:38 +0600

    create new project

---
+++ src/test/java/sberqa/SimpleTest.java
@@ -0,0 +1,11 @@
+package sberqa;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class SimpleTest {
+    @Test
+    void simpleTest() {
+        Assertions.assertTrue(true);
+    }
+}
